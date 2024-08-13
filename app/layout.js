3d98774fae62692db234a37084b8ca89707b88be
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-08-13 14:09:53 -0400

    Added gtag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-P4YRJ8WZ07"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-P4YRJ8WZ07'});
+          `}
+      </Script>
       <head>
         <link rel="icon" href="../favicon.ico" />
         <meta name="description" content={metadata.description} />
